e1430d878d2c75a8d675a0869f97734396bef68e
Daria Semenii <dashasv76@gmail.com>
2025-04-08 21:13:56 +0300

    First pract

--- js/pract.js
+++ js/pract.js
@@ -1,3 +1,21 @@
-"use strict";
-let num = 5
-console.log(num + +"5");
+'use strict';
+
+const numberOfFilms = +prompt('Скільки фільмів переглянуто?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх фільмів?', ''),
+      b = prompt('На скільки оціните його?', ''),
+      c = prompt('Один з останніх фільмів?', ''),
+      d = prompt('На скільки оціните його?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
